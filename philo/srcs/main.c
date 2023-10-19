d1e5052e5f9112b4c235265b063208a9c0e0f9b4
Farengar Darkhawk <fdarkhaw@c2s18d110.42wolfsburg.de>
2023-10-19 10:48:18 +0200

    refactored

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jg <jg@student.42.fr>                      +#+  +:+       +#+        */
+/*   By: fdarkhaw <fdarkhaw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 23:48:42 by fdarkhaw          #+#    #+#             */
-/*   Updated: 2022/06/12 12:47:18 by jg               ###   ########.fr       */
+/*   Updated: 2022/06/12 12:47:18 by fdarkhaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,12 +40,12 @@ int	main(int ac, char **av)
 	t_data	data;
 
 	if (ac < 5 || ac > 6)
-		return (ft_error("Wrong number of arguments"));
+		return (ft_error("Wrong number of arguments.\nProgram take the following arguments: number_of_philosophers time_to_die time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]"));
 	if (validation(&data, av))//ok
 		return (1);
-	if (init_philosophers(&data) || init_forks(&data))//ок
+	if (init_philosophers(&data) || init_forks(&data))//ok
 		return (1);
-	if (start_game(&data))//разобраться с breaker
+	if (start_game(&data))//ok
 		return (1);
 	finish_game(&data);
 	return (0);
